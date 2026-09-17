81c97915a8297da7e7f87b1a0f71d7379ec3774c
pmindlab <240002383+pmindlab@users.noreply.github.com>
2026-09-17 16:50:14 +0200

    Search v2: export semantic provider

--- app/providers/__init__.py
+++ app/providers/__init__.py
@@ -1,5 +1,5 @@
 from .gbif import GBIFProvider, TaxonHit
 from .domain import VerisignRdapProvider, DomainResult
 from .brand import BrandScreenProvider, BrandResult
-
 from .language import LanguageProvider, LanguageHit
+from .semantic import SemanticWordProvider, SemanticHit
